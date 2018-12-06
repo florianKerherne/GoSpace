db2aeb54b4c5453fc1e9cdc27598f0f71b8977b0
fkerhern <fkerhern@LFR026348.corp.capgemini.com>
2018-12-06 15:34:14 +0100

    t

--- GoSpaceBackend/src/main/java/backend/GoSpaceBackendApplication.java
+++ GoSpaceBackend/src/main/java/backend/GoSpaceBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoSpaceBackendApplication {
-
+	int ccoo;
 	public static void main(String[] args) {
 		SpringApplication.run(GoSpaceBackendApplication.class, args);
 	}
