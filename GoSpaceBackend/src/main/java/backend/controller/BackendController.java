75b64109eba118d63ad7b9505f6e4471f5fb1e52
fkerhern <fkerhern@LFR026348.corp.capgemini.com>
2018-12-11 15:49:50 +0100

    Ajout class Utilisateur + interface

--- GoSpaceBackend/src/main/java/backend/controller/BackendController.java
+++ GoSpaceBackend/src/main/java/backend/controller/BackendController.java
@@ -3,12 +3,15 @@ package backend.controller;
 import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import backend.modelInterface.UtilisateurRepository;
 
 @RestController()
 @RequestMapping("/")
@@ -18,6 +21,10 @@ public class BackendController {
 	
 	public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
 
+	@Autowired
+    private UtilisateurRepository userRepository;
+	
+	
 	//fonction de récuperation des informations pour Jira 
 	@RequestMapping(value  = "about", method = RequestMethod.GET)
 	public @ResponseBody String getInfo() {
--- GoSpaceBackend/src/main/java/backend/model/Utilisateur.java
+++ GoSpaceBackend/src/main/java/backend/model/Utilisateur.java
@@ -1,5 +1,80 @@
 package backend.model;
 
-public class Utilisateur {
+import javax.persistence.*;
 
+@Entity
+@Table(name="Utilisateur")
+public class Utilisateur {
+	
+	// PrimaryKey
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String MDP;
+    private Boolean admin;
+
+    protected Utilisateur() {}
+    
+    public Utilisateur(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMDP() {
+		return MDP;
+	}
+
+	public void setMDP(String mDP) {
+		MDP = mDP;
+	}
+
+	public Boolean getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Boolean admin) {
+		this.admin = admin;
+	}
 }
+
+
+
+
+
