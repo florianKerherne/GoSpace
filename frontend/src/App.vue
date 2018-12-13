a800d300ed060d1c61525df3f15ffdebb9edf3ac
thomas <thomas@172.28.168.75>
2018-12-13 17:45:21 +0100

    connexion

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Bonjour</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/connexion">Connexion</router-link>
     </div>
     <router-view/>
   </div>
