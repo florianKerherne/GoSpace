98bc967f00927e1993fec3846db7c8ab43ceb1c6
fkerhern <fkerhern@LFR026348.corp.capgemini.com>
2018-12-13 16:03:01 +0100

    fix pb lancement backend

--- GoSpaceBackend/src/main/java/backend/GoSpaceBackendApplication.java
+++ GoSpaceBackend/src/main/java/backend/GoSpaceBackendApplication.java
@@ -1,12 +1,15 @@
 package backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class GoSpaceBackendApplication implements WebMvcConfigurer {
 	
 	public static void main(String[] args) {
