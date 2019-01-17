78371a4dc9db0882c0377a71555f47d6291984d9
thomas <thomas@172.28.141.129>
2019-01-17 05:54:06 +0100

    voyage

--- frontend/src/components/ArticleVoyage.vue
+++ frontend/src/components/ArticleVoyage.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="articleVoyage">
-    <b-btn @click="callRestService()">CALL Spring Boot REST backend service</b-btn>
-    <p>go to the space !
-      <br>Paris - Mars
-    </p>
-    <h3>999€99</h3>
+  <div class="ArticleVoyage">
+    <div>
+      <div id="photo"></div>
+      <div id="descriptif">
+        <h3>Titre</h3>
+        <h5>Zone de départ</h5>
+        <p>Descriptif
+          <br>Paris - Mars
+        </p>
+      </div>
+    </div>
+    <div id="piedDePage">
+      <h5>999€99</h5>
+      <b-btn @click="callRestService()">GO to the SPACE</b-btn>
+    </div>
   </div>
 </template>
 
@@ -13,7 +22,7 @@
 import { AXIOS } from "./http-common";
 
 export default {
-  name: "articleVoyage",
+  name: "ArticleVoyage",
 
   data() {
     return {
@@ -36,3 +45,33 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.ArticleVoyage {
+  padding: 30px;
+  color: #321168;
+  background-color: rgb(141, 26, 106);
+  margin-left:110px;
+}
+#photo {
+  padding: 30px;
+  color: #6e6e6e;
+  background-color: rgb(71, 71, 71);
+  margin-left:110px;
+  float: left;
+}
+#descriptif {
+  padding: 30px;
+  color: #eb0606;
+  background-color: rgb(14, 168, 168);
+  float: center;
+}
+#piedDePage {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #eb0606;
+  background-color: rgb(0, 255, 55);
+}
+</style>
--- frontend/src/views/ListVoyage.vue
+++ frontend/src/views/ListVoyage.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="listVoyage">
-    <b-btn @click="callRestService()">CALL Spring Boot REST backend service</b-btn>
-    <p>For a guide and recipes on how to configure / customize this project,
-      <br>check out the
+    <b-form inline>
+      <b-form-group id="InputPlaneteGroup"
+                    label="Planete:"
+                    label-for="InputPlanete">
+        <b-form-input id="InputPlanete"
+                      type="text"
+                      required
+                      placeholder="Sélectionner une planete">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="InputLieuGroup"
+                    label="Lieu:"
+                    label-for="InputLieu">
+        <b-form-input id="InputLieu"
+                      type="text"
+                      required
+                      placeholder="saisir un lieu">
+        </b-form-input>
+      </b-form-group>
+      <b-button variant="primary" @click="callRestService()">Rechercher</b-button>
+    </b-form>
+    <p>vos voyage :
+      <br>
     </p>
+    <ArticleVoyage/>
     <h3>Installed CLI Plugins</h3>
   </div>
 </template>
@@ -11,16 +32,20 @@
 
 <script>
 import { AXIOS } from "../components/http-common";
-
+import ArticleVoyage from '@/components/ArticleVoyage.vue';
+//import HelloWorld from '@/components/HelloWorld.vue';
 export default {
   name: "listVoyage",
-
+  components: {
+    ArticleVoyage
+  },
   data() {
     return {
       posts: [],
       errors: []
     };
   },
+  
   methods: {
     // Fetches posts when the component is created.
     callRestService() {
